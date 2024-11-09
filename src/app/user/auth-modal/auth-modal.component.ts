ad66718465ee68dccd4d56af3b9c44919aa804f5
Fernando Henrique Alves <fernando.everywhere@gmail.com>
2024-11-09 18:40:30 -0300

    login and register forms separated into components

--- src/app/user/auth-modal/auth-modal.component.ts
+++ src/app/user/auth-modal/auth-modal.component.ts
@@ -3,11 +3,19 @@ import { ModalComponent } from "../../modal/modal.component";
 import { ModalService } from '../../services/modal.service';
 import { TabsContainerComponent } from "../../tabs-container/tabs-container.component";
 import { TabComponent } from '../../tab/tab.component';
+import { LoginComponent } from '../login/login.component';
+import { RegisterComponent } from '../register/register.component';
 
 @Component({
   selector: 'app-auth-modal',
   standalone: true,
-  imports: [ModalComponent, TabsContainerComponent, TabComponent],
+  imports: [
+    ModalComponent,
+    TabsContainerComponent,
+    TabComponent,
+    LoginComponent,
+    RegisterComponent
+  ],
   templateUrl: './auth-modal.component.html',
   styleUrl: './auth-modal.component.css'
 })
