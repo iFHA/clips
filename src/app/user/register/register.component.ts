1fa8d4586551a7aa92eb23a77452007f062dc241
Fernando Henrique Alves <fernando.everywhere@gmail.com>
2024-11-09 19:22:35 -0300

    bind the register form with the formgroup and formcontrollers

--- src/app/user/register/register.component.ts
+++ src/app/user/register/register.component.ts
@@ -1,12 +1,20 @@
 import { Component } from '@angular/core';
+import { ReactiveFormsModule, FormGroup, FormControl } from '@angular/forms';
 
 @Component({
   selector: 'app-register',
   standalone: true,
-  imports: [],
+  imports: [ReactiveFormsModule],
   templateUrl: './register.component.html',
   styleUrl: './register.component.css'
 })
 export class RegisterComponent {
-
+  registerForm = new FormGroup({
+    name: new FormControl(''),
+    email: new FormControl(''),
+    age: new FormControl(''),
+    password: new FormControl(''),
+    confirmPassword: new FormControl(''),
+    phoneNumber: new FormControl('')
+  });
 }
