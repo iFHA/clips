9722fed8817e9b250e93204b5c62f0a58a708711
Fernando Henrique Alves <fernando.everywhere@gmail.com>
2024-11-11 20:44:10 -0300

    login form with FormsModule

--- src/app/user/login/login.component.ts
+++ src/app/user/login/login.component.ts
@@ -1,13 +1,26 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
-import { ReactiveFormsModule, FormGroup } from '@angular/forms';
+import { FormsModule } from '@angular/forms';
+import { AlertComponent } from '../../alert/alert.component';
 
 @Component({
   selector: 'app-login',
   standalone: true,
-  imports: [ReactiveFormsModule],
+  imports: [FormsModule, CommonModule, AlertComponent],
   templateUrl: './login.component.html',
   styleUrl: './login.component.css'
 })
 export class LoginComponent {
-
+  credentials = {
+    email: '',
+    password: ''
+  }
+  showAlert = false;
+  alertMsg = 'Por favor, aguarde! realizando o login.';
+  alertColor = 'blue';
+  login(): void {
+    this.showAlert = true;
+    this.alertMsg = 'Por favor, aguarde! realizando o login.';
+    this.alertColor = 'blue';
+  }
 }
