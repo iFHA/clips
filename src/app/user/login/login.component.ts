ce892db8c090b14852987ef103f29246b6033ee5
Fernando Henrique Alves <fernando.everywhere@gmail.com>
2024-11-10 21:54:56 -0300

    register and input components

--- src/app/user/login/login.component.ts
+++ src/app/user/login/login.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
+import { ReactiveFormsModule, FormGroup } from '@angular/forms';
 
 @Component({
   selector: 'app-login',
   standalone: true,
-  imports: [],
+  imports: [ReactiveFormsModule],
   templateUrl: './login.component.html',
   styleUrl: './login.component.css'
 })
--- src/app/user/register/register.component.ts
+++ src/app/user/register/register.component.ts
@@ -1,20 +1,49 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
-import { ReactiveFormsModule, FormGroup, FormControl } from '@angular/forms';
+import {
+  ReactiveFormsModule,
+  FormGroup,
+  FormControl,
+  Validators
+} from '@angular/forms';
+import { InputComponent } from '../../input/input.component';
 
 @Component({
   selector: 'app-register',
   standalone: true,
-  imports: [ReactiveFormsModule],
+  imports: [ReactiveFormsModule, CommonModule, InputComponent],
   templateUrl: './register.component.html',
   styleUrl: './register.component.css'
 })
 export class RegisterComponent {
   registerForm = new FormGroup({
-    name: new FormControl(''),
-    email: new FormControl(''),
-    age: new FormControl(''),
-    password: new FormControl(''),
-    confirmPassword: new FormControl(''),
-    phoneNumber: new FormControl('')
+    name: new FormControl('', [
+      Validators.required,
+      Validators.minLength(3)
+    ]),
+    email: new FormControl('', [
+      Validators.required,
+      Validators.email
+    ]),
+    age: new FormControl('', [
+      Validators.required,
+      Validators.min(18),
+      Validators.max(120)
+    ]),
+    password: new FormControl('', [
+      Validators.required,
+      Validators.pattern(/^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[a-zA-Z]).{8,}$/gm)
+    ]),
+    confirmPassword: new FormControl('', [
+      Validators.required
+    ]),
+    phoneNumber: new FormControl('', [
+      Validators.required,
+      Validators.minLength(10),
+      Validators.maxLength(11)
+    ])
   });
+  register(): void {
+    console.log('register called');
+  }
 }
