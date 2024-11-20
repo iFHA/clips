87383b7e9dbbd74c98e93e018fc86cddfb27963f
Fernando Henrique Alves <fernando.everywhere@gmail.com>
2024-11-20 15:18:17 -0300

    add routerLink for about page

--- src/app/nav/nav.component.ts
+++ src/app/nav/nav.component.ts
@@ -2,11 +2,12 @@ import { Component, OnInit } from '@angular/core';
 import { ModalService } from '../services/modal.service';
 import { AuthService } from '../services/auth.service';
 import { CommonModule } from '@angular/common';
+import { RouterLink, RouterLinkActive } from '@angular/router';
 
 @Component({
   selector: 'app-nav',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, RouterLink, RouterLinkActive],
   templateUrl: './nav.component.html',
   styleUrl: './nav.component.css'
 })
