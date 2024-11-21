e3fa1a757de4ce66cc749d921d86061d5540091e
Fernando Henrique Alves <fernando.everywhere@gmail.com>
2024-11-21 12:46:09 -0300

    using sorting query param

--- src/app/video/manage/manage.component.ts
+++ src/app/video/manage/manage.component.ts
@@ -1,12 +1,27 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
 
 @Component({
   selector: 'app-manage',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './manage.component.html',
   styleUrl: './manage.component.css'
 })
 export class ManageComponent {
-
+  videoOrder = '1';
+  constructor(
+    private router: Router,
+    private route: ActivatedRoute
+  ) {
+    this.route.queryParamMap.subscribe(paramMap => this.videoOrder = paramMap.get('sort') ?? '1');
+  }
+  sort(event: Event) {
+    const { value } = (event.target as HTMLSelectElement);
+    this.router.navigate([], {
+      relativeTo: this.route,
+      queryParams: {sort: value}
+    });
+  }
 }
