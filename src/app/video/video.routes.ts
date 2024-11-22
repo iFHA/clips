69b3d00c315ca0df94aa917b5fae3c4ca441b032
Fernando Henrique Alves <fernando.everywhere@gmail.com>
2024-11-21 23:16:01 -0300

    add guard to redirect unauthorized user to home

--- src/app/video/video.routes.ts
+++ src/app/video/video.routes.ts
@@ -1,21 +1,28 @@
 import { Routes } from '@angular/router';
 import { ManageComponent } from './manage/manage.component';
 import { UploadComponent } from './upload/upload.component';
+import { AuthGuard, redirectUnauthorizedTo } from '@angular/fire/auth-guard';
+
+const redirectUnauthorizedToHome = () => redirectUnauthorizedTo('/');
 
 export const VIDEO_ROUTES: Routes = [
   {
     path: "manage",
     component: ManageComponent,
     data: {
-      authOnly: true
-    }
+      authOnly: true,
+      authGuardPipe: redirectUnauthorizedToHome
+    },
+    canActivate: [AuthGuard]
   },
   {
     path: "upload",
     component: UploadComponent,
     data: {
-      authOnly: true
-    }
+      authOnly: true,
+      authGuardPipe: redirectUnauthorizedToHome
+    },
+    canActivate: [AuthGuard]
   },
   {
     path: "manage-clips",// assumindo que antes havia esse caminho
