e23a08c80884bc02abc9cf6411a4c28c4a52e330
Fernando Henrique Alves <fernando.everywhere@gmail.com>
2024-11-30 00:27:36 -0300

    add drag and drop support

--- src/app/video/upload/upload.component.ts
+++ src/app/video/upload/upload.component.ts
@@ -1,13 +1,18 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 import { EventBlockerDirective } from '../../directives/event-blocker.directive';
 
 @Component({
   selector: 'app-upload',
-  imports: [EventBlockerDirective],
+  imports: [EventBlockerDirective, CommonModule],
   standalone: true,
   templateUrl: './upload.component.html',
   styleUrl: './upload.component.css'
 })
 export class UploadComponent {
+  isDragover = false;
 
+  storeFile($event: Event) {
+    this.isDragover = false;
+  }
 }
