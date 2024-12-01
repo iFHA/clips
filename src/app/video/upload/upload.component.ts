e2cd5dffd99219aa1a74fa08f5fc8736dbab694a
Fernando Henrique Alves <fernando.everywhere@gmail.com>
2024-12-01 19:03:17 -0300

    the upload form

--- src/app/video/upload/upload.component.ts
+++ src/app/video/upload/upload.component.ts
@@ -1,10 +1,12 @@
 import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 import { EventBlockerDirective } from '../../directives/event-blocker.directive';
+import { ReactiveFormsModule, FormControl, FormGroup, Validators } from '@angular/forms';
+import { InputComponent } from '../../input/input.component';
 
 @Component({
   selector: 'app-upload',
-  imports: [EventBlockerDirective, CommonModule],
+  imports: [EventBlockerDirective, CommonModule, ReactiveFormsModule, InputComponent],
   standalone: true,
   templateUrl: './upload.component.html',
   styleUrl: './upload.component.css'
@@ -12,6 +14,18 @@ import { EventBlockerDirective } from '../../directives/event-blocker.directive'
 export class UploadComponent {
   isDragover = false;
   nextStep = false;
+
+  title = new FormControl<string>('', {
+    validators:[
+      Validators.required,
+      Validators.minLength(3)
+    ],
+    nonNullable: true
+  });
+
+  uploadForm = new FormGroup({
+    title: this.title
+  });
 
   file: File | null = null;
 
@@ -23,7 +37,11 @@ export class UploadComponent {
     if(!this.file || this.file.type !== 'video/mp4') {
       return;
     }
-
+    this.title.setValue(this.file.name.replace(/\.[^/.]+$/, ''));
     this.nextStep = true;
+  }
+
+  uploadFile() {
+    console.log('File uploaded');
   }
 }
